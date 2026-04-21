b72dfca328ea8c44a8688f86b23e287901486047
Thiago Candia <candiathiago123@gmail.com>
2026-04-21 14:45:37 -0300

    header-nav

--- client/src/Components/Nav.jsx
+++ client/src/Components/Nav.jsx
@@ -1,176 +1,221 @@
-import React, { useState } from "react";
-import logoNav from "../assets/Img/logonav.png";
-import bag from "../assets/Img/bag.png";
-import { Link } from "react-router-dom";
-import { Icons } from "../Assets/Icons/Icons";
+import React from "react"
+import logoNav from "../assets/Img/logonav.png"
+import bag from "../assets/Img/bag.png"
+import { Link } from "react-router-dom"
+import { Icons } from "../Assets/Icons/Icons"
 
 const Nav = ({ onSearchClick }) => {
   return (
-    <>
-      <header>
-        <nav>
-          {/* NAVBAR OPTIONS */}
-          <div className="nav__item">
-            <ul>
-              <li>
-                <button className="btn-config btn__nav">
-                  <div className="link__nav__box">
-                    <Link href="/">
-                    <span className="text btn__nav__text"
-                        >women
-                    </span>
-                    </Link>
-                    <ul className="sublinks__nav__box">
-                      <li className="sublink-box-nav">
-                        <Link
-                          href="/"
-                          className="btn-config btn-sublink-box-nav"
-                        >
-                          <span>new arrivals</span>
-                        </Link>
-                      </li>
-                      <li>
-                        <span>face covers</span>
-                      </li>
-                      <li>
-                        <span>bottoms</span>
-                      </li>
-                      <li>
-                        <span>top</span>
-                      </li>
-                      <li>
-                        <span>dresses</span>
-                      </li>
-                      <li>
-                        <span>outwear</span>
-                      </li>
-                      <li>
-                        <span>plus size</span>
-                      </li>
-                      <li>
-                        <span>accessories</span>
-                      </li>
-                      <li>
-                        <span>sale</span>
-                      </li>
-                    </ul>
-                  </div>
-                </button>
-              </li>
-              <li>
-                <button className="btn-config btn__nav">
-                  <div className="link__nav__box">
-                    <Link href="/">
-                      <span className="text btn__nav__text">men</span>
-                    </Link>
-                    <ul className="sublinks__nav__box">
-                      <li className="sublinks__box">
-                        <span>new arrivals</span>
-                      </li>
-                      <li className="sublinks__box">
-                        <span>face covers</span>
-                      </li>
-                      <li className="sublinks__box">
-                        <span>bottoms</span>
-                      </li>
-                      <li className="sublinks__box">
-                        <span>top</span>
-                      </li>
-                      <li className="sublinks__box">
-                        <span>dresses</span>
-                      </li>
-                      <li className="sublinks__box">
-                        <span>outwear</span>
-                      </li>
-                      <li className="sublinks__box">
-                        <span>plus size</span>
-                      </li>
-                      <li className="sublinks__box">
-                        <span>accessories</span>
-                      </li>
-                      <li className="sublinks__box">
-                        <span>sale</span>
-                      </li>
-                    </ul>
-                  </div>
-                </button>
-              </li>
-              <li>
-                <button className="btn-config btn__nav">
-                  <div className="link__nav__box">
-                    <Link href="/">
-                      <span className="text btn__nav__text">darkstreet</span>
-                    </Link>
-                    <ul className="sublinks__nav__box">
-                      <li className="sublinks__box">
-                        <span>new arrivals</span>
-                      </li>
-                      <li className="sublinks__box">
-                        <span>face covers</span>
-                      </li>
-                      <li className="sublinks__box">
-                        <span>bottoms</span>
-                      </li>
-                      <li className="sublinks__box">
-                        <span>top</span>
-                      </li>
-                      <li className="sublinks__box">
-                        <span>dresses</span>
-                      </li>
-                      <li className="sublinks__box">
-                        <span>outwear</span>
-                      </li>
-                      <li className="sublinks__box">
-                        <span>plus size</span>
-                      </li>
-                      <li className="sublinks__box">
-                        <span>accessories</span>
-                      </li>
-                    </ul>
-                  </div>
-                </button>
-              </li>
-              <li>
-                <button className="btn-config btn__nav">
-                  <a href="/">
-                    <span className="text btn__nav__text">sale</span>
-                  </a>
-                </button>
-              </li>
-            </ul>
-          </div>
 
-          {/* IMG NAV */}
-          <div className="nav__item">
-            <Link to={"/"} className="logo__nav">
-              <img src={logoNav} />
-            </Link>
-          </div>
-
-          {/* CARRITO */}
-          <div className="nav__item nav__item__bag">
-            <button
-              className="btn-config nav__item__bag__btn"
-              onClick={onSearchClick}
-            >
-              <i className="nav__item__bag__btn__icon">
-                <Icons.MagnifyingGlass />
-              </i>
-            </button>
-            <span>
-              <a href="">Account</a>
-            </span>
-            <div className="nav__bag">
-              <span>Tote </span>
-              <div className="nav__bag__box__img">
-                <img className="nav__bag__img" src={bag} alt="" />
+    <header className="header">
+      <nav className="nav">
+        <div className="nav__menu">
+          <ul className="nav__list">
+            <li className="nav__item">
+              <div className="nav__dropdown">
+                <Link to="/" className="nav__link">
+                  <span className="nav__text">women</span>
+                </Link>
+                <ul className="nav__submenu">
+                  <li className="nav__submenu-item">
+                    <Link to="/collections/women/new-arrivals" className="nav__submenu-link">
+                      <span className="nav__submenu-text">new arrivals</span>
+                    </Link>
+                  </li>
+                  <li className="nav__submenu-item">
+                    <Link to="/collections/women/face-covers" className="nav__submenu-link">
+                      <span className="nav__submenu-text">face covers</span>
+                    </Link>
+                  </li>
+                  <li className="nav__submenu-item">
+                    <Link to="/collections/women/bottoms" className="nav__submenu-link">
+                      <span className="nav__submenu-text">bottoms</span>
+                    </Link>
+                  </li>
+                  <li className="nav__submenu-item">
+                    <Link to="/collections/women/top" className="nav__submenu-link">
+                      <span className="nav__submenu-text">top</span>
+                    </Link>
+                  </li>
+                  <li className="nav__submenu-item">
+                    <Link to="/collections/women/dresses" className="nav__submenu-link">
+                      <span className="nav__submenu-text">dresses</span>
+                    </Link>
+                  </li>
+                  <li className="nav__submenu-item">
+                    <Link to="/collections/women/outwear" className="nav__submenu-link">
+                      <span className="nav__submenu-text">outwear</span>
+                    </Link>
+                  </li>
+                  <li className="nav__submenu-item">
+                    <Link to="/collections/women/plus-size" className="nav__submenu-link">
+                      <span className="nav__submenu-text">plus size</span>
+                    </Link>
+                  </li>
+                  <li className="nav__submenu-item">
+                    <Link to="/collections/women/accessories" className="nav__submenu-link">
+                      <span className="nav__submenu-text">accessories</span>
+                    </Link>
+                  </li>
+                  <li className="nav__submenu-item">
+                    <Link to="/collections/women/sale" className="nav__submenu-link">
+                      <span className="nav__submenu-text">sale</span>
+                    </Link>
+                  </li>
+                </ul>
               </div>
-            </div>
-          </div>
-        </nav>
-      </header>
-    </>
-  );
-};
+            </li>
 
-export default Nav;
+            {/* MEN */}
+            <li className="nav__item">
+              <div className="nav__dropdown">
+                <Link to="/" className="nav__link">
+                  <span className="nav__text">men</span>
+                </Link>
+                <ul className="nav__submenu">
+                  <li className="nav__submenu-item">
+                    <Link to="/collections/men/new-arrivals" className="nav__submenu-link">
+                      <span className="nav__submenu-text">new arrivals</span>
+                    </Link>
+                  </li>
+                  <li className="nav__submenu-item">
+                    <Link to="/collections/men/face-covers" className="nav__submenu-link">
+                      <span className="nav__submenu-text">face covers</span>
+                    </Link>
+                  </li>
+                  <li className="nav__submenu-item">
+                    <Link to="/collections/men/bottoms" className="nav__submenu-link">
+                      <span className="nav__submenu-text">bottoms</span>
+                    </Link>
+                  </li>
+                  <li className="nav__submenu-item">
+                    <Link to="/collections/men/top" className="nav__submenu-link">
+                      <span className="nav__submenu-text">top</span>
+                    </Link>
+                  </li>
+                  <li className="nav__submenu-item">
+                    <Link to="/collections/men/dresses" className="nav__submenu-link">
+                      <span className="nav__submenu-text">dresses</span>
+                    </Link>
+                  </li>
+                  <li className="nav__submenu-item">
+                    <Link to="/collections/men/outwear" className="nav__submenu-link">
+                      <span className="nav__submenu-text">outwear</span>
+                    </Link>
+                  </li>
+                  <li className="nav__submenu-item">
+                    <Link to="/collections/men/plus-size" className="nav__submenu-link">
+                      <span className="nav__submenu-text">plus size</span>
+                    </Link>
+                  </li>
+                  <li className="nav__submenu-item">
+                    <Link to="/collections/men/accessories" className="nav__submenu-link">
+                      <span className="nav__submenu-text">accessories</span>
+                    </Link>
+                  </li>
+                  <li className="nav__submenu-item">
+                    <Link to="/collections/men/sale" className="nav__submenu-link">
+                      <span className="nav__submenu-text">sale</span>
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            </li>
+
+            {/* DARKSTREET */}
+            <li className="nav__item">
+              <div className="nav__dropdown">
+                <Link to="/" className="nav__link">
+                  <span className="nav__text">darkstreet</span>
+                </Link>
+                <ul className="nav__submenu">
+                  <li className="nav__submenu-item">
+                    <Link to="/collections/darkstreet/new-arrivals" className="nav__submenu-link">
+                      <span className="nav__submenu-text">new arrivals</span>
+                    </Link>
+                  </li>
+                  <li className="nav__submenu-item">
+                    <Link to="/collections/darkstreet/face-covers" className="nav__submenu-link">
+                      <span className="nav__submenu-text">face covers</span>
+                    </Link>
+                  </li>
+                  <li className="nav__submenu-item">
+                    <Link to="/collections/darkstreet/bottoms" className="nav__submenu-link">
+                      <span className="nav__submenu-text">bottoms</span>
+                    </Link>
+                  </li>
+                  <li className="nav__submenu-item">
+                    <Link to="/collections/darkstreet/top" className="nav__submenu-link">
+                      <span className="nav__submenu-text">top</span>
+                    </Link>
+                  </li>
+                  <li className="nav__submenu-item">
+                    <Link to="/collections/darkstreet/dresses" className="nav__submenu-link">
+                      <span className="nav__submenu-text">dresses</span>
+                    </Link>
+                  </li>
+                  <li className="nav__submenu-item">
+                    <Link to="/collections/darkstreet/outwear" className="nav__submenu-link">
+                      <span className="nav__submenu-text">outwear</span>
+                    </Link>
+                  </li>
+                  <li className="nav__submenu-item">
+                    <Link to="/collections/darkstreet/plus-size" className="nav__submenu-link">
+                      <span className="nav__submenu-text">plus size</span>
+                    </Link>
+                  </li>
+                  <li className="nav__submenu-item">
+                    <Link to="/collections/darkstreet/accessories" className="nav__submenu-link">
+                      <span className="nav__submenu-text">accessories</span>
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            </li>
+
+            {/* SALE */}
+            <li className="nav__item">
+              <div className="nav__dropdown">
+                <Link to="/sale" className="nav__link">
+                  <span className="nav__text">sale</span>
+                </Link>
+              </div>
+            </li>
+          </ul>
+        </div>
+
+        {/* LOGO */}
+        <div className="nav__logo">
+          <Link to="/">
+            <img src={logoNav} alt="TRIPP NYC Logo" className="nav__logo-img" />
+          </Link>
+        </div>
+
+        {/* ACTIONS */}
+        <div className="nav__actions">
+          <button
+            className="nav__action-btn"
+            onClick={onSearchClick}
+            aria-label="Open search"
+          >
+            <Icons.MagnifyingGlass />
+          </button>
+
+          <Link to="/account" className="nav__action-link">
+            Account
+          </Link>
+
+          <Link to="/cart" className="nav__cart">
+            <span className="nav__cart-label">Tote</span>
+            <span className="nav__cart-separator">—</span>
+            <div className="nav__cart-icon">
+              <img src={bag} alt="Shopping bag" className="nav__cart-img" />
+            </div>
+          </Link>
+        </div>
+      </nav>
+    </header>
+  )
+}
+
+export default Nav
--- client/src/Screens/ProductScreen.jsx
+++ client/src/Screens/ProductScreen.jsx
@@ -51,149 +51,149 @@ export const ProductScreen = () => {
 
         <main className="product">
         {/* Galería de imágenes */}
-        <section className="product__gallery">
-            <button
-                    className="product__gallery-nav product__gallery-nav--prev"
-                    onClick={handlePrevImage}
-                    aria-label="Previous image"
-                >
-                ‹
-            </button>
-
-            <div className="product__gallery-main">
-                <img
-                    src={productImages[currentImageIndex]}
-                    alt={product.name}
-                    className="product__gallery-image"
-                />
-                <button className="product__gallery-view-more">
-                    UNISEX - View more images
-                </button>
-            </div>
-
-            <button
-                className="product__gallery-nav product__gallery-nav--next"
-                onClick={handleNextImage}
-                aria-label="Next image"
-            >
-                ›
-            </button>
-        </section>
-
-        {/* Información del producto */}
-        <section className="product__info">
-          {/* Header con favorito y breadcrumb */}
-            <div className="product__header">
+            <section className="product__gallery">
                 <button
-                    className="product__favorite"
-                    onClick={() => setIsFavorite(!isFavorite)}
-                    aria-label="Add to favorites"
+                        className="product__gallery-nav product__gallery-nav--prev"
+                        onClick={handlePrevImage}
+                        aria-label="Previous image"
+                    >
+                    ‹
+                </button>
+        
+                    <div className="product__gallery-main">
+                    <img
+                        src={productImages[currentImageIndex]}
+                        alt={product.name}
+                        className="product__gallery-image"
+                    />
+                    <button className="product__gallery-view-more">
+                        UNISEX - View more images
+                    </button>
+                </div>
+        
+                    <button
+                    className="product__gallery-nav product__gallery-nav--next"
+                    onClick={handleNextImage}
+                    aria-label="Next image"
                 >
-                    {isFavorite ? "❤" : "♡"}
-            </button>
-
-            <span className="product__sku">
-                No. {product.sku || "AF3921MB-GNB-XXS"}
-            </span>
-
-            <nav className="product__breadcrumb">
-                <Link to="/" className="product__breadcrumb-link">
-                    Home
-                </Link>
-                <span className="product__breadcrumb-separator">
-                    \
+                    ›
+                </button>
+            </section>
+        
+                {/* Información del producto */}
+            <section className="product__info">
+              {/* Header con favorito y breadcrumb */}
+                <div className="product__header">
+                    <button
+                        className="product__favorite"
+                        onClick={() => setIsFavorite(!isFavorite)}
+                        aria-label="Add to favorites"
+                    >
+                        {isFavorite ? "❤" : "♡"}
+                </button>
+        
+                    <span className="product__sku">
+                    No. {product.sku || "AF3921MB-GNB-XXS"}
                 </span>
-                <Link to="/collections" className="product__breadcrumb-link">
-                    New Arrivals Homepage
-                </Link>
-            </nav>
-            </div>
-
-            {/* Título y precio */}
-            <div className="product__title-section">
-                <h1 className="product__title">{product.name}</h1>
-                <p className="product__price">${product.price}</p>
-            </div>
-
-            <hr className="product__divider" />
-
-            {/* Selección de talla */}
-            <div className="product__size-section">
-            <h2 className="product__size-label">
-                Size
-            </h2>
-
-            <div className="product__size-grid">
-                {sizes.map((size) => (
-                <button
-                    key={size}
-                    className={`product__size-button ${
-                    selectedSize === size
-                        ? "product__size-button--selected"
-                        : ""
-                    }`}
-                    onClick={() => setSelectedSize(size)}
-                >
-                    {size}
-                </button>
-                ))}
-            </div>
-
-            <div className="product__actions">
-                <button
-                    className="product__action-button product__action-button--primary"
-                    disabled={!selectedSize}
-                >
-                Select a size
-                </button>
-
-                <button className="product__action-button product__action-button--secondary">
-                    Need Help?
-                </button>
-            </div>
-
-            <button className="product__size-chart">Size Chart</button>
-            </div>
-
-            <hr className="product__divider" />
-
-            {/* Descripción del producto */}
-            <div className="product__description">
-            <p className="product__description-text">
-                {product.description ||
-                "These green slime bleach wash pants zip off into shorts and feature removable chains, adjustable ankles, D-rings, and deep pockets."}
-            </p>
-
-            <div className="product__details">
-                <p className="product__details-item">
-                    <strong>WOMAN IS WEARING X-SMALL</strong>
+        
+                    <nav className="product__breadcrumb">
+                    <Link to="/" className="product__breadcrumb-link">
+                        Home
+                    </Link>
+                    <span className="product__breadcrumb-separator">
+                        \
+                    </span>
+                    <Link to="/collections" className="product__breadcrumb-link">
+                        New Arrivals Homepage
+                    </Link>
+                </nav>
+                </div>
+        
+                    {/* Título y precio */}
+                <div className="product__title-section">
+                    <h1 className="product__title">{product.name}</h1>
+                    <p className="product__price">${product.price}</p>
+                </div>
+        
+                    <hr className="product__divider" />
+        
+                    {/* Selección de talla */}
+                <div className="product__size-section">
+                <h2 className="product__size-label">
+                    Size
+                </h2>
+        
+                    <div className="product__size-grid">
+                    {sizes.map((size) => (
+                    <button
+                        key={size}
+                        className={`product__size-button ${
+                        selectedSize === size
+                            ? "product__size-button--selected"
+                            : ""
+                        }`}
+                        onClick={() => setSelectedSize(size)}
+                    >
+                        {size}
+                    </button>
+                    ))}
+                </div>
+                
+                    <div className="product__actions">
+                    <button
+                        className="product__action-button product__action-button--primary"
+                        disabled={!selectedSize}
+                    >
+                    Select a size
+                    </button>
+                
+                        <button className="product__action-button product__action-button--secondary">
+                        Need Help?
+                    </button>
+                </div>
+                
+                    <button className="product__size-chart">Size Chart</button>
+                </div>
+                
+                    <hr className="product__divider" />
+                
+                    {/* Descripción del producto */}
+                <div className="product__description">
+                <p className="product__description-text">
+                    {product.description ||
+                    "These green slime bleach wash pants zip off into shorts and feature removable chains, adjustable ankles, D-rings, and deep pockets."}
                 </p>
-                <p className="product__details-item">
-                    <strong>MAN IS WEARING MEDIUM</strong>
-                </p>
-                <p className="product__details-item">
-                    <strong>SIZING BASED ON MENS FIT</strong>
-                </p>
-
-                <ul className="product__features">
-                    <li className="product__features-item">
-                        – Refer to Unisex Darkstreet Pant - Size Chart (Based on Men's
-                        Sizing)
-                    </li>
-                    <li className="product__features-item">
-                        – Drawstring and adjustable waist buckles allow for a tighter
-                        fit on the waist
-                    </li>
-                    <li className="product__features-item">
-                        – 100% Cotton.
-                    </li>
-                    <li className="product__features-item">
-                        - Hand wash cold. Lay flat to dry.
-                    </li>
-                </ul>
-            </div>
-            </div>
-        </section>
+                    
+                <div     className="product__details">
+                    <p className="product__details-item">
+                        <strong>WOMAN IS WEARING X-SMALL</strong>
+                    </p>
+                    <p className="product__details-item">
+                        <strong>MAN IS WEARING MEDIUM</strong>
+                    </p>
+                    <p className="product__details-item">
+                        <strong>SIZING BASED ON MENS FIT</strong>
+                    </p>
+                    
+                        <ul className="product__features">
+                        <li className="product__features-item">
+                            – Refer to Unisex Darkstreet Pant - Size Chart (Based on Men's
+                            Sizing)
+                        </li>
+                        <li className="product__features-item">
+                            – Drawstring and adjustable waist buckles allow for a tighter
+                            fit on the waist
+                        </li>
+                        <li className="product__features-item">
+                            – 100% Cotton.
+                        </li>
+                        <li className="product__features-item">
+                            - Hand wash cold. Lay flat to dry.
+                        </li>
+                    </ul>
+                </div>
+                </div>
+            </section>
         </main>
 
         <footer>
