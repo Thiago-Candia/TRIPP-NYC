eda92333140a69a39138ab7cfd55e3f1ddc88933
Thiago Candia <candiathiago123@gmail.com>
2026-05-05 13:36:33 -0300

    refactoring

---
+++ tripp/cart/services.py
@@ -0,0 +1,16 @@
+from .models import Cart
+from users.models import CustomUser
+
+
+def get_or_create_cart(request):
+    if request.user.is_authenticated and isinstance(request.user, CustomUser):
+        cart, _ = Cart.objects.get_or_create(user=request.user)
+        return cart
+
+    session_id = request.session.session_key
+    if not session_id:
+        request.session.create()
+        session_id = request.session.session_key
+
+    cart, _ = Cart.objects.get_or_create(session_id=session_id)
+    return cart
